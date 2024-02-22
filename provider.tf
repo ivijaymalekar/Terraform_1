f77d970be771fb1db162baa16d198ed1686f77d9
Vijay Malekar <84920002+ivijaymalekar@users.noreply.github.com>
2024-02-22 17:40:05 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,8 @@
 terraform {
   required_providers {
     aws = {
+    aws_access_key_id = AKIA6IZ5O2OHORYU7DEG
+    aws_secret_access_key = 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
       source  = "hashicorp/aws"
       version = "~>5.37 "
     }
